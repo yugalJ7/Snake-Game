1ea117d7f40ef9f860c94caf395b6baf70730164
gingHUB <jyugal180.jy@gmail.com>
2024-01-18 16:40:08 +0530

    update music path

--- snake-game/snakeGame-script.js
+++ snake-game/snakeGame-script.js
@@ -16,10 +16,15 @@ food = {
 };
 
 
-const foodSound = new Audio('../music/food.mp3');
-const gameOversound = new Audio('../music/gameover.mp3');
-const moveSound = new Audio('../music/move.mp3');
-const musicSound = new Audio('../music/music.mp3');
+// const foodSound = new Audio('../music/food.mp3');
+// const gameOversound = new Audio('../music/gameover.mp3');
+// const moveSound = new Audio('../music/move.mp3');
+// const musicSound = new Audio('../music/music.mp3');
+
+const foodSound = new Audio('music/food.mp3');
+const gameOversound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
 
 let speed = 5;
 let lastPainttime = 0;
